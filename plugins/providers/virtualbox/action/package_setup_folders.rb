55357bfb860e6aec0be46e1965dd248a8c62dbea
Johannes Kastl <kastl@b1-systems.de>
2018-11-16 21:12:43 +0100

    fix vbox package boo#1044087, added by robert.munteanu@gmail.com on Sun Aug 13 19:07:06 UTC 2017
    Signed-off-by: Johannes Kastl <kastl@b1-systems.de>

--- plugins/providers/virtualbox/action/package_setup_folders.rb
+++ plugins/providers/virtualbox/action/package_setup_folders.rb
@@ -1,6 +1,6 @@
 require "fileutils"
 
-require_relative "../../../../lib/vagrant/action/general/package_setup_folders"
+require "vagrant/action/general/package"
 
 module VagrantPlugins
   module ProviderVirtualBox
